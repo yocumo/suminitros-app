5a3baf6eafd802fe9b436f155373f4f83a8fdabc
yocumo <analitica.med2@eiasa.com.co>
2024-07-03 10:01:04 -0500

    f

--- app.py
+++ app.py
@@ -1,57 +1,58 @@
 import os
 import runapi
 import data
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for, send_file)
-
-
-
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    url_for,
+    send_file,
+)
 
 app = Flask(__name__)
 
 
-
-@app.route('/')
+@app.route("/")
 def index():
-   print('Request for index page received')
-   return render_template('home.html')
-   
+    print("Request for index page received")
+    return render_template("home.html")
 
-# @app.route('/home', methods=['GET'])
-# def home():
-#    try:   
-#       return render_template('home.html')
-   
-   # except ValueError as e:
-   #          print(f'Error: {e} -- redirecting to /home')
-   #          return redirect(url_for('index'))
 
-@app.route('/hello', methods=['GET'])
+@app.route("/hello", methods=["GET"])
 def hello():
-   
-   try:   
-      return render_template('hello.html')
-   
-   except ValueError as e:
-            print(f'Error: {e} -- redirecting to /hello')
-            return redirect(url_for('index'))
-   
-@app.route('/consulta', methods=['POST'])
+
+    try:
+        return render_template("hello.html")
+
+    except ValueError as e:
+        print(f"Error: {e} -- redirecting to /hello")
+        return redirect(url_for("index"))
+
+
+@app.route("/consulta", methods=["POST"])
 def consulta():
 
-   name = str(request.form.get('name'))
-   if name:
-      print(f'Request for consulta page received with name={name}')
-      try:
-         df, user_name, eq = runapi.FindUser(name)
-      
-         if df is not None:
-            styled_df = df.style.applymap(color_diff, subset=['diff'])
-            html_table = styled_df.to_html()
+    name = str(request.form.get("name"))
+    if name:
+        print(f"Request for consulta page received with name={name}")
+        try:
+            df, user_name, eq = runapi.FindUser(name)
 
-            return render_template('home.html', consulta_result=html_table, name=name, user=user_name, eq=eq)
-         else:
-            return '''<html lang="es">
+            if df is not None:
+                styled_df = df.style.applymap(color_diff, subset=["diff"])
+                html_table = styled_df.to_html()
+
+                return render_template(
+                    "home.html",
+                    consulta_result=html_table,
+                    name=name,
+                    user=user_name,
+                    eq=eq,
+                )
+            else:
+                return """<html lang="es">
                      <head>
                         <meta charset="UTF-8">
                         <title>Hola Mundo</title>
@@ -59,26 +60,28 @@ def consulta():
                      <body>
                         <h1>¡Ups! al parecer no hay datos para mostrar</h1>
                      </body>
-                     </html>'''
-      except ValueError as e:
-            print(f'Error: {e} -- redirecting to /hello')
-            return redirect(url_for('hello'))
-      
-   else:
-      print('Request for hello page received with no name or blank name -- redirecting')
-      return redirect(url_for('index'))
-   
+                     </html>"""
+        except ValueError as e:
+            print(f"Error: {e} -- redirecting to /hello")
+            return redirect(url_for("hello"))
 
-@app.route('/bodega', methods=['GET'])
+    else:
+        print(
+            "Request for hello page received with no name or blank name -- redirecting"
+        )
+        return redirect(url_for("index"))
+
+
+@app.route("/bodega", methods=["GET"])
 def bodega():
-   try:
-      df = data.suministros()
-      if df is not None:
-         df_html = df.to_html() #(classes='table table-bordered styled-table', index=True, escape=False)
-         return render_template('almacen.html', df_html=df_html)
-      
-      else:
-         return '''<html lang="es">
+    try:
+        df = data.suministros()
+        if df is not None:
+            df_html = df.to_html()
+            return render_template("almacen.html", df_html=df_html)
+
+        else:
+            return """<html lang="es">
                      <head>
                         <meta charset="UTF-8">
                         <title>Hola Mundo</title>
@@ -86,16 +89,16 @@ def bodega():
                      <body>
                         <h1>¡Ups! al parecer no hay datos para mostrar</h1>
                      </body>
-                     </html>'''
-   except ValueError as e:
-            print(f'Error: {e} -- redirecting to /hello')
-            return redirect(url_for('hello'))
-   
+                     </html>"""
+    except ValueError as e:
+        print(f"Error: {e} -- redirecting to /hello")
+        return redirect(url_for("hello"))
+
+
 def color_diff(val):
-    color = 'red' if val < 1 else ''
-    return f'background-color: {color}'
+    color = "red" if val < 1 else ""
+    return f"background-color: {color}"
 
 
-
-if __name__ == '__main__':
-   app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
--- runapi.py
+++ runapi.py
@@ -2,113 +2,119 @@ import requests
 import pandas as pd
 
 
-
 headers = {
     "Accept": "application/json",
     "Content-Type": "application/json",
     "Authorization": "Token 52cca904974d60be2dd4f5d3024bcb5d2135ec8a",
-    "Organization": "164"
-    }
+    "Organization": "164",
+}
+
+stock_maximo = pd.read_csv("stock_maximos.csv")
+stock_maximo["CODIGO"] = stock_maximo["CODIGO"].astype(str)
 
 
-stock_maximo = pd.read_csv('stock_maximos.csv')
-stock_maximo['CODIGO'] = stock_maximo['CODIGO'].astype(str)
-#print(stock_maximo.info())
-
-
-#conexión a la api 
+# conexión a la api
 def RunApi(URL):
     api_url = URL
     try:
-    # Realiza una solicitud GET a la API
-        response = requests.get(api_url,headers=headers)
-
-        if response.status_code in [200,201]:
-            #print(response.json())
+        response = requests.get(api_url, headers=headers)
+        if response.status_code in [200, 201]:
             return response.json()
         else:
             data = response.json()
             print("Datos de la API:", data)
             return ("Datos de la API:", data)
-               
+
     except requests.exceptions.RequestException as e:
         print(f"Error al realizar la solicitud a la API: {str(e)}")
-        return (f"Error al realizar la solicitud a la API: {str(e)}")
-        
+        return f"Error al realizar la solicitud a la API: {str(e)}"
+
     except Exception as e:
         print(f"Ocurrió un error: {str(e)}")
-        return (f"Ocurrió un error: {str(e)}")
+        return f"Ocurrió un error: {str(e)}"
 
-#ETL dataframe cartera tecnicos y cruce con csv stock_maximos = tope
-def consulta(user_id):  
-    api = 'https://app.sytex.io/api/materialstock/?staff='+str(user_id)+'&limit=100'
+
+# ETL dataframe cartera tecnicos y cruce con csv stock_maximos = tope
+def consulta(user_id):
+    api = "https://app.sytex.io/api/materialstock/?staff=" + str(user_id) + "&limit=100"
     ln = RunApi(api)
 
     try:
-        if ln['results']:
-            df = ln['results']
+        if ln["results"]:
+            df = ln["results"]
             data = []
 
             for i in df:
-                data.append({
-                        'Tipo': i['material']['material_type']['name'],
-                        'Código de material': i['material_code'],
-                        'Nombre de material': i['material_name'],
-                        #'Número de serie': i['serial_number'],
-                        'Ubicación': i['location']['name'],
-                        'Cantidad': i['quantity'],
-                        #'Atributos': i['attributes'],
-                })
-                #insert.append(data)
+                data.append(
+                    {
+                        "Tipo": i["material"]["material_type"]["name"],
+                        "Código de material": i["material_code"],
+                        "Nombre de material": i["material_name"],
+                        "Ubicación": i["location"]["name"],
+                        "Cantidad": i["quantity"],
+                    }
+                )
 
-            df2 = pd.DataFrame(data).sort_values(by='Tipo')
+            df2 = pd.DataFrame(data).sort_values(by="Tipo")
+            user_name = df2.iloc[0]["Ubicación"]
+            df_g = (
+                df2.groupby("Código de material")
+                .agg(
+                    {
+                        "Tipo": "first",
+                        "Nombre de material": "first",
+                        "Cantidad": "sum",
+                    }
+                )
+                .reset_index()
+            )
 
-            user_name = df2.iloc[0]['Ubicación']
-            
-            df_g = df2.groupby('Código de material').agg({
-                'Tipo': 'first',
-                'Nombre de material': 'first',  # O cualquier otra agregación que tenga sentido
-                'Cantidad': 'sum',
-            }).reset_index()
+            df_g = df_g.sort_values(by="Tipo")
 
-            df_g = df_g.sort_values(by='Tipo')
-
-            merged_df = pd.merge(df_g, stock_maximo, left_on='Código de material', right_on='CODIGO', how='left')
-            df_stock = merged_df.drop(columns=['CODIGO', 'MATERIAL'])
+            merged_df = pd.merge(
+                df_g,
+                stock_maximo,
+                left_on="Código de material",
+                right_on="CODIGO",
+                how="left",
+            )
+            df_stock = merged_df.drop(columns=["CODIGO", "MATERIAL"])
             df_stock.fillna(0, inplace=True)
-            #df_stock['TECNICOS'] = df_stock['TECNICOS'].astype(int)
-            df_stock = df_stock.rename(columns={'TECNICOS': 'Tope'}).astype({'Tope': int})
-            #df_stock['diff'] = (df_stock['Tope'] - df_stock['Cantidad']).astype(int)
+            df_stock = df_stock.rename(columns={"TECNICOS": "Tope"}).astype(
+                {"Tope": int}
+            )
 
-            df_stock['diff'] = df_stock.apply(lambda i: i['Tope'] - i['Cantidad'] if i['Tope'] != 0 else i['Cantidad'], axis=1)
-            equipos = df_stock.loc[df_stock['Tipo'] == 'Equipos', 'Cantidad'].sum()
-
-            #print(df_stock.info())
-            #df_stock.to_csv('dataframe.csv', index=False)
+            df_stock["diff"] = df_stock.apply(
+                lambda i: (
+                    i["Tope"] - i["Cantidad"] if i["Tope"] != 0 else i["Cantidad"]
+                ),
+                axis=1,
+            )
+            equipos = df_stock.loc[df_stock["Tipo"] == "Equipos", "Cantidad"].sum()
 
             return df_stock, user_name, equipos
         else:
-            print("User_id no existe "+user_id)
-            return("Usuer_id no existe "+user_id)
-        
-    except Exception as e:
-        print(f"error : {str(e)}"+ " al encontrar al usuario "+ str(user_id))
-        return (f"error : {str(e)}"+ " al encontrar al usuario "+ str(user_id))
+            print("User_id no existe " + user_id)
+            return "Usuer_id no existe " + user_id
 
-#Encontrar codigo de usuario=tecnico para encontrar los datos de sus cartera en def consulta
+    except Exception as e:
+        print(f"error : {str(e)}" + " al encontrar al usuario " + str(user_id))
+        return f"error : {str(e)}" + " al encontrar al usuario " + str(user_id)
+
+
+# Encontrar codigo de usuario=tecnico para encontrar los datos de sus cartera en def consulta
 def FindUser(User):
-    api = "https://app.sytex.io/api/staff/?q="+str(User)
+    api = "https://app.sytex.io/api/staff/?q=" + str(User)
     User = RunApi(api)
-    try :
-        if User['results'][0]['id']:
-            user_id = User['results'][0]['id']
+    try:
+        if User["results"][0]["id"]:
+            user_id = User["results"][0]["id"]
             send_ui = consulta(user_id)
             return send_ui
         else:
-            print("Usuario no existe "+User)
-            return("Usuario no existe "+User)
-                    
+            print("Usuario no existe " + User)
+            return "Usuario no existe " + User
+
     except Exception as e:
-        print(f"error : {str(e)}"+ " al encontrar al usuario "+ str(User))
-        return (f"error : {str(e)}"+ " al encontrar al usuario "+ str(User))
-    
+        print(f"error : {str(e)}" + " al encontrar al usuario " + str(User))
+        return f"error : {str(e)}" + " al encontrar al usuario " + str(User)
