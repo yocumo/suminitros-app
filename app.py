c3d2993c9623bf2000274213644cd6c311c7176c
yocumo <analitica.med2@eia.com.co>
2024-07-09 09:22:14 -0500

    server port

--- app.py
+++ app.py
@@ -114,5 +114,5 @@ def color_diff(val):
 
 
 if __name__ == "__main__":
-    # serve(app, host="0.0.0.0", port=8080)
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=8080)
+    # app.run(debug=True)
